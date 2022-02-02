8cd6c1e14f2e2eb24870623977996d833e8933e4
venugopalallada <98863182+venugopalallada@users.noreply.github.com>
2022-02-01 22:35:57 -0500

    Add files via upload

---
+++ Spring-Kafka-Consumer/src/main/java/example/kafka/spring/consumer/KafkaConfiguration.java
@@ -0,0 +1,65 @@
+package example.kafka.spring.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.yaml.snakeyaml.events.NodeEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//@EnableKafka
+//@Configuration
+public class KafkaConfiguration {
+
+/*    @Value("${kafka.group}")
+    private String group;
+
+    @Value("${kafka.servers}")
+    private String servers;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(getBaseConfig());
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, NodeEvent> nodeEventConsumerFactory() {
+        Map<String, Object> config = getBaseConfig();
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(NodeEvent.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, NodeEvent> nodeEventKafkaListenerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, NodeEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(nodeEventConsumerFactory());
+        return factory;
+    }
+
+    private Map<String, Object> getBaseConfig() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        return config;
+    }*/
+
+}
